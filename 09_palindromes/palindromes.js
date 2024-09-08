9adf581158d9d226ba702ce41146c4094999370c
Muhamed Jonuzovic <155450175+jonuzovic-muhamed@users.noreply.github.com>
2024-09-08 12:08:31 +0200

    Add solution to exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,36 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+    let filteredOutText = text.replace(/[^a-zA-Z0-9]/g, '');
+    let stringLength = filteredOutText.length;
+
+    let firstStringHalf = '';
+    let secondsStringHalf = '';
+
+    // string length even number
+    if (stringLength % 2 == 0) {
+
+        firstStringHalf = filteredOutText.substring(0, (stringLength / 2)).toLowerCase();
+
+        secondsStringHalf = filteredOutText.substring((stringLength / 2), stringLength).split("").reverse().join("").toLowerCase();
+
+    // string length uneven number
+    } else {
+
+        firstStringHalf = filteredOutText.substring(0, Math.ceil(stringLength / 2)).toLowerCase();
+
+        secondsStringHalf = filteredOutText.substring(Math.floor(stringLength / 2), stringLength).split("").reverse().join("").toLowerCase();
+
+    }
+
+    if (firstStringHalf == secondsStringHalf) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
+
+palindromes('racecar!')
 
 // Do not edit below this line
 module.exports = palindromes;
