4288cf06aa989404a858167e1728cc031b3a15fa
Muhamed Jonuzovic <155450175+jonuzovic-muhamed@users.noreply.github.com>
2024-09-09 19:26:36 +0200

    Add solution to exercises 10 - 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
 
+    if (typeof number !== 'number') {
+        number = parseInt(number)
+    } else {
+        number = number
+    }
+
+    if (number <= 1) return number;
+    return fibonacci(number - 1) + fibonacci(number - 2);
 };
 
-// Do not edit below this line
 module.exports = fibonacci;
+ 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,23 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest;
+    let currentOldestAge;
+    for (let person of people) {
+        let age;
+        if (person.yearOfDeath == undefined) {
+            age = new Date().getFullYear - person.yearOfBirth;
+            if (age > currentOldestAge) {
+                currentOldestAge = age;
+                oldest = person.name;
+            }
+        } else {
+            age = yearOfDeath - yearOfBirth;
+            if (age > currentOldestAge) {
+                currentOldestAge = age;
+                oldest = person.name;
+            }
+        }
+    }
+    return oldest;
 };
 
 // Do not edit below this line
